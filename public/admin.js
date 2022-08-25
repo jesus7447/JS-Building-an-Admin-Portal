827f97367b469391359f3d0060d565d558960469
User <j_camacho8@yahoo.com>
2022-08-24 20:13:01 -0700

    first commit

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,56 @@
 
 // Your Code Here
+async function main(){
+   let res = await fetch('http://localhost:3001/listBooks')
+
+   let books = await res.json()
+
+   books.forEach(displayBook)
+}
+
+function displayBook(book){
+   let rootDiv = document.querySelector('#root')
+
+   let li = document.createElement('li')
+   li.textContent = book.title
+
+   let input = document.createElement('input')
+   input.value = book.quantity
+
+   let button = document.createElement('button')
+   button.textContent = 'Save'
+
+   let delButton = document.createElement('button')
+   delButton.textContent = 'Delete'
+
+
+   button.addEventListener('click', function(){
+        fetch('http://localhost:3001/updateBook',{
+            method: 'PATCH',
+            headers: { 'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                id: book.id,
+                quantity: input.value
+            })
+        })
+   })
+
+
+   delButton.addEventListener('click', function(){
+       fetch('http://localhost:3001/removeBook/{bookId}', {
+           method:'DELETE',
+           headers:{'Contenet-Type': 'application/json'},
+           body: null
+       })
+       
+   })
+
+   
+
+   li.append(input, button, delButton)
+   rootDiv.append(li)
+
+
+}
+
+main()
